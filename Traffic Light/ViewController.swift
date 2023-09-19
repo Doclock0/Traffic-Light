e216540a81e2e00c73c78eb30f9e99a4ae785d3c
Doclock <Doctoracretg@mail.ru>
2023-09-19 06:26:30 +0300

    Traffic Light

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSign: UIView!
+    
+    @IBOutlet var yellowSign: UIView!
+    
+    @IBOutlet var greenSign: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 10
+        redSign.layer.cornerRadius = 45
+        yellowSign.layer.cornerRadius = 45
+        greenSign.layer.cornerRadius = 45
+        
+        redSign.alpha = 0.3
+        yellowSign.alpha = 0.3
+        greenSign.alpha = 0.3
+    
         // Do any additional setup after loading the view.
     }
-
-
+    var nextSign = 1
+    @IBAction func makeButtonAction() {
+        switch nextSign {
+            case 1:
+            startButton.setTitle("NEXT", for: .normal)
+            redSign.alpha = 1
+            greenSign.alpha = 0.3
+            nextSign = 2
+            case 2:
+            redSign.alpha = 0.3
+            yellowSign.alpha = 1
+            nextSign = 3
+            case 3:
+            yellowSign.alpha = 0.3
+            greenSign.alpha = 1
+            nextSign = 1
+            default:
+                break
+            }
+        }
+    
 }
-
